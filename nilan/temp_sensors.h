73a7e805feab531c9ac347eaf4a396a262cc968e
Jeppe Merrild Andersen <jeppe.merrildandersen@marel.com>
2021-02-10 15:54:32 +0100

    Based on the pzem implementation, which use modbus. Starting with temp sensors only. Compiles, but makes bootloop :-S

---
+++ nilan/temp_sensors.h
@@ -0,0 +1,81 @@
+#include "esphome.h"
+//#include "sensor.h"
+//#include "component.h"
+//#include "esphome/core/component.h"
+//#include "esphome/components/modbus/modbus.h"
+
+//#define REGISTERCOUNT 17
+//#define MAXREGSIZE 26
+//#define SENDINTERVAL 30000 // normally set to 180000 milliseconds = 3 minutes. Define as you like
+//#define VENTSET 1003
+//#define RUNSET 1001
+//#define MODESET 1002
+//#define TEMPSET 1004
+
+#define REGSIZE 23
+
+/*enum reqtypes
+{
+  reqtemp = 0,
+  reqalarm,
+  reqtime,
+  reqcontrol,
+  reqspeed,
+  reqairtemp,
+  reqairflow,
+  reqairheat,
+  requser,
+  requser2,
+  reqinfo,
+  reqinputairtemp,
+  reqapp,
+  reqoutput,
+  reqdisplay1,
+  reqdisplay2,
+  reqdisplay,
+  reqmax
+};*/
+
+static int16_t rsbuffer[MAXREGSIZE];
+static const char *TAG = "nilan_temperature";
+
+class TempSensors : public PollingComponent, public esphome::modbus::ModbusDevice
+{
+  public:
+    void set_controller_temp_sensor(Sensor *controller_temp_sensor) { controller_temp_sensor_ = controller_temp_sensor; }
+    void set_exhaust_temp_sensor(Sensor *exhaust_temp_sensor) { exhaust_temp_sensor_ = exhaust_temp_sensor; }
+    void set_outlet_temp_sensor(Sensor *outlet_temp_sensor) { outlet_temp_sensor_ = outlet_temp_sensor; }
+    void set_inlet_temp_sensor(Sensor *inlet_temp_sensor) { inlet_temp_sensor_ = inlet_temp_sensor; }
+    void set_outdoor_temp_sensor(Sensor *outdoor_temp_sensor) { outdoor_temp_sensor_ = outdoor_temp_sensor; }
+   
+    void update() override {};
+
+    void on_modbus_data(const std::vector<uint8_t> &data) override {
+      ESP_LOGD(TAG, "Data received...");
+      if (data.size() < 16) {
+        ESP_LOGW(TAG, "Invalid data size received");
+        return;
+      }
+    }
+
+    void dump_config() override  {
+      ESP_LOGCONFIG(TAG, "Nilan temperature:");
+      ESP_LOGCONFIG(TAG, " Address: 0x%02X", this->address_);
+      LOG_SENSOR("", "Controller", this->controller_temp_sensor_);
+      LOG_SENSOR("", "Exhaust", this->exhaust_temp_sensor_);
+      LOG_SENSOR("", "Outlet", this->outlet_temp_sensor_);
+      LOG_SENSOR("", "Inlet", this->inlet_temp_sensor_);
+      LOG_SENSOR("", "Outdoor", this->outdoor_temp_sensor_);
+    }
+
+    float get_setup_priority() const override { 
+      return esphome::setup_priority::AFTER_WIFI; 
+    }
+
+  protected:
+    Sensor *controller_temp_sensor_;
+    Sensor *exhaust_temp_sensor_;
+    Sensor *outlet_temp_sensor_;
+    Sensor *inlet_temp_sensor_;
+    Sensor *outdoor_temp_sensor_;
+};
