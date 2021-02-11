95e927bd23ccd62e00f5c5b956fa9ed296921c5c
Jeppe Merrild Andersen <jeppe.merrildandersen@marel.com>
2021-02-11 10:07:37 +0100

    No bootloop anymore \o/ Set update interval to 3 seconds for now. Added dummy data to verify that sensors are updating; seems to work. Still no data from modbus....

--- nilan/temp_sensors.h
+++ nilan/temp_sensors.h
@@ -12,6 +12,7 @@
 //#define MODESET 1002
 //#define TEMPSET 1004
 
+#define READINTERVAL 3000
 #define REGSIZE 23
 
 /*enum reqtypes
@@ -42,6 +43,14 @@ static const char *TAG = "nilan_temperature";
 class TempSensors : public PollingComponent, public esphome::modbus::ModbusDevice
 {
   public:
+    TempSensors() : PollingComponent(READINTERVAL) {
+      dummyData = 0;
+      controller_temp_sensor_->publish_state(dummyData);
+      exhaust_temp_sensor_->publish_state(dummyData);
+      outlet_temp_sensor_->publish_state(dummyData);
+      inlet_temp_sensor_->publish_state(dummyData);
+      outdoor_temp_sensor_->publish_state(dummyData);
+    };
     //void set_controller_temp_sensor(Sensor *controller_temp_sensor) { controller_temp_sensor_ = controller_temp_sensor; }
     //void set_exhaust_temp_sensor(Sensor *exhaust_temp_sensor) { exhaust_temp_sensor_ = exhaust_temp_sensor; }
     //void set_outlet_temp_sensor(Sensor *outlet_temp_sensor) { outlet_temp_sensor_ = outlet_temp_sensor; }
@@ -49,7 +58,16 @@ class TempSensors : public PollingComponent, public esphome::modbus::ModbusDevic
     //void set_outdoor_temp_sensor(Sensor *outdoor_temp_sensor) { outdoor_temp_sensor_ = outdoor_temp_sensor; }
    
     void update() override {
-      ESP_LOGD(TAG, "Update...");
+      //auto read_result = this->read();
+      //ESP_LOGD(TAG, "Update. Read: %d", read_result);
+      
+      dummyData++;
+      controller_temp_sensor_->publish_state(dummyData);
+      exhaust_temp_sensor_->publish_state(dummyData+100);
+      outlet_temp_sensor_->publish_state(dummyData*1.1);
+      inlet_temp_sensor_->publish_state(dummyData*-1);
+      outdoor_temp_sensor_->publish_state(dummyData-100);
+      dump_config();
     };
 
     void on_modbus_data(const std::vector<uint8_t> &data) override {
@@ -80,4 +98,7 @@ class TempSensors : public PollingComponent, public esphome::modbus::ModbusDevic
     Sensor *outlet_temp_sensor_ = new Sensor();
     Sensor *inlet_temp_sensor_ = new Sensor();
     Sensor *outdoor_temp_sensor_ = new Sensor();
+    
+  private:
+    int dummyData;
 };
