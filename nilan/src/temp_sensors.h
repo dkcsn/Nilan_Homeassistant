036bf6e1918913cc1b3d19fa0e603e03b0784050
Jopand <jopand47@gmail.com>
2021-02-11 21:27:43 +0100

    Wired Modbus up with UART. Added Modbus as parent to ModbusDevice... Getting there, but still no data :-S

--- nilan/src/temp_sensors.h
+++ nilan/src/temp_sensors.h
@@ -12,7 +12,7 @@
 //#define MODESET 1002
 //#define TEMPSET 1004
 
-#define READINTERVAL 3000
+#define READINTERVAL 10000
 #define REGSIZE 23
 
 /*enum reqtypes
@@ -43,32 +43,36 @@ static const char *TAG = "nilan_temperature";
 class TempSensors : public PollingComponent, public esphome::modbus::ModbusDevice
 {
   public:
-    TempSensors() : PollingComponent(READINTERVAL) {
+    TempSensors(Modbus *modbus) : PollingComponent(READINTERVAL) {
+      set_parent(modbus);
+      set_address(30);
+
       dummyData = 0;
       controller_temp_sensor_->publish_state(dummyData);
       exhaust_temp_sensor_->publish_state(dummyData);
       outlet_temp_sensor_->publish_state(dummyData);
       inlet_temp_sensor_->publish_state(dummyData);
       outdoor_temp_sensor_->publish_state(dummyData);
-    };
+    }
     //void set_controller_temp_sensor(Sensor *controller_temp_sensor) { controller_temp_sensor_ = controller_temp_sensor; }
     //void set_exhaust_temp_sensor(Sensor *exhaust_temp_sensor) { exhaust_temp_sensor_ = exhaust_temp_sensor; }
     //void set_outlet_temp_sensor(Sensor *outlet_temp_sensor) { outlet_temp_sensor_ = outlet_temp_sensor; }
     //void set_inlet_temp_sensor(Sensor *inlet_temp_sensor) { inlet_temp_sensor_ = inlet_temp_sensor; }
     //void set_outdoor_temp_sensor(Sensor *outdoor_temp_sensor) { outdoor_temp_sensor_ = outdoor_temp_sensor; }
-   
+
     void update() override {
       //auto read_result = this->read();
       //ESP_LOGD(TAG, "Update. Read: %d", read_result);
-      
+
       dummyData++;
       controller_temp_sensor_->publish_state(dummyData);
-      exhaust_temp_sensor_->publish_state(dummyData+100);
-      outlet_temp_sensor_->publish_state(dummyData*1.1);
-      inlet_temp_sensor_->publish_state(dummyData*-1);
-      outdoor_temp_sensor_->publish_state(dummyData-100);
-      dump_config();
-    };
+      //exhaust_temp_sensor_->publish_state(dummyData+100);
+      //outlet_temp_sensor_->publish_state(dummyData*1.1);
+      //inlet_temp_sensor_->publish_state(dummyData*-1);
+      //outdoor_temp_sensor_->publish_state(dummyData-100);
+      
+      //dump_config();
+    }
 
     void on_modbus_data(const std::vector<uint8_t> &data) override {
       ESP_LOGD(TAG, "Data received...");
@@ -79,13 +83,13 @@ class TempSensors : public PollingComponent, public esphome::modbus::ModbusDevic
     }
 
     void dump_config() override  {
-      ESP_LOGCONFIG(TAG, "Nilan temperature:");
-      ESP_LOGCONFIG(TAG, " Address: 0x%02X", this->address_);
+      //ESP_LOGCONFIG(TAG, "Nilan temperature:");
+      //ESP_LOGCONFIG(TAG, " Address: 0x%02X", this->address_);
       LOG_SENSOR("", "Controller", this->controller_temp_sensor_);
-      LOG_SENSOR("", "Exhaust", this->exhaust_temp_sensor_);
-      LOG_SENSOR("", "Outlet", this->outlet_temp_sensor_);
-      LOG_SENSOR("", "Inlet", this->inlet_temp_sensor_);
-      LOG_SENSOR("", "Outdoor", this->outdoor_temp_sensor_);
+      //LOG_SENSOR("", "Exhaust", this->exhaust_temp_sensor_);
+      //LOG_SENSOR("", "Outlet", this->outlet_temp_sensor_);
+      //LOG_SENSOR("", "Inlet", this->inlet_temp_sensor_);
+      //LOG_SENSOR("", "Outdoor", this->outdoor_temp_sensor_);
     }
 
     float get_setup_priority() const override { 
@@ -98,7 +102,7 @@ class TempSensors : public PollingComponent, public esphome::modbus::ModbusDevic
     Sensor *outlet_temp_sensor_ = new Sensor();
     Sensor *inlet_temp_sensor_ = new Sensor();
     Sensor *outdoor_temp_sensor_ = new Sensor();
-    
+
   private:
     int dummyData;
 };
